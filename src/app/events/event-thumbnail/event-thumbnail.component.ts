67b7d434c62c275c41566ff2e6cca910e4de77d0
Nelson SMG <nelsonsmalfitano@gmail.com>
2024-02-03 12:59:39 -0300

    Using styles

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
   styles: [
+    `
+      .pad-left { margin-left: 10px }
+      .well div { color: #bbb }
+    `
   ]
 })
 export class EventThumbnailComponent implements OnInit {
 
   @Input() event: any
+  // @Output() eventClick = new EventEmitter()
+
+  someProperty: any = "some value"
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // handleClickMe() {
+  //   this.eventClick.emit('foo')
+  // }
+
+  // logFoo() {
+  //   console.log('foo')
+  // }
 
 }
