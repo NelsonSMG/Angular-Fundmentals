637d1ab320be5df1b06cfe0f45010ca8e9664cc5
NelsonSMG <nelsonsmalfitano@gmail.com>
2024-02-03 22:11:01 -0300

    data/Event bindings and directives

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -5,8 +5,11 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './event-thumbnail.component.html',
   styles: [
     `
+      .green {color: #003300 !important}
+      .bold {font-weight: bold}
       .pad-left { margin-left: 10px }
       .well div { color: #bbb }
+      .thumbnail {min-height: 210px}
     `
   ]
 })
@@ -29,5 +32,23 @@ export class EventThumbnailComponent implements OnInit {
   // logFoo() {
   //   console.log('foo')
   // }
+
+  getStartTimeClass(){
+    const isEarlyStart = this.event?.time === '8:00 am' && this.event?.time === '8:00 am'
+
+    if (this.event?.time === '8:00 am' && this.event?.time === '8:00 am') {
+      return "green bold"
+    }
+    return ""
+    // return {green: isEarlyStart, bold: isEarlyStart}
+  }
+
+  //if using ngStyle
+  getStartTimeStyle(){
+    if (this.event?.time === '8:00 am' && this.event?.time === '8:00 am') {
+      return {color: '#003300', 'font-weight': 'bold' }
+    }
+    return {}
+  }
 
 }
