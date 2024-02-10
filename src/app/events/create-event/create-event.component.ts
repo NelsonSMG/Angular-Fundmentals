9255583ec1e698f514178b2ae182a14f2287f8d3
NelsonSMG <nelsonsmalfitano@gmail.com>
2024-02-10 00:46:02 -0300

    Create event form

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -1,22 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { EventService } from '../shared/event.service';
 
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
   styles: [
+    `
+    em {float:right; color: #E05C65; padding-left:10px}
+    .error input {background-color: #E3C3C5}
+    .error ::-webkit-input-placeholder {color: #999}
+    .error ::-moz-placeholder {color: #999}
+    .error :-moz-placeholder {color: #999}
+    .error :ms-input-placeholder {color: #999}
+
+  `
   ]
 })
 export class CreateEventComponent implements OnInit {
 
+
+  newEvent: any
+  isDirty: boolean = true
+
   constructor(
-    private router: Router
+    private router: Router,
+    private eventService: EventService
   ) { }
 
   ngOnInit(): void {
+    // this.newEvent = {
+    //   name: "Ng Spectacular",
+    //   date: '8/8/2023',
+    //   time: '10am',
+    //   price: 799.99,
+    //   location: {
+    //     address: '23423',
+    //     city: 'Felicity',
+    //     country: 'Angular'
+    //   },
+    //   onlineUrl: 'http://mochiron',
+    //   imageUrl: 'http://mochiron/mochiron.png',
+    // }
   }
 
-  cancel() {
+  cancel(){
+    this.router.navigate(['/events'])
+  }
+
+  saveEvent(formValues: any){
+    console.log(formValues)
+    this.eventService.saveEvent(formValues)
+    this.isDirty = false
     this.router.navigate(['/events'])
   }
 
--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { IEvent } from './event.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,22 +9,36 @@ export class EventService {
 
   constructor() { }
 
-  getEvents() {
-    return Events
+  getEvents() :Observable<IEvent[]> {
+    let subject = new Subject<IEvent[]>()
+    setTimeout( ()=> { subject.next(events); subject.complete(); }, 100 )
+
+    return subject
   }
 
   getEvent(id: number) {
-    return Events.find(event => event.id == id)
+    return events.find(event => event.id === id)
   }
-  
+
+  saveEvent(event: any){
+    event.id = 999
+    event.sessions = []
+    events.push(event)
+
+  }
+
+  updateEvent(event: IEvent) {
+    let index = events.findIndex( e => e.id == event.id )
+    events[index] = event
+  }
 
 }
 
-const Events = [
+const events: IEvent[] = [
   {
     id: 1,
     name: 'Angular Connect',
-    date: '9/26/2036',
+    date: new Date('9/26/2036'),
     time: '10:00 am',
     price: 599.99,
     imageUrl: '/assets/images/angularconnect-shield.png',
@@ -100,11 +116,11 @@ const Events = [
   {
     id: 2,
     name: 'ng-nl',
-    date: '4/15/2037',
+    date: new Date('4/15/2037'),
     time: '9:00 am',
     price: 950.00,
     imageUrl: '/assets/images/ng-nl.png',
-    onlineUrl: 'ohyaohya',
+    onlineUrl: 'clubpenguin',
     sessions: [
       {
         id: 1,
@@ -156,7 +172,7 @@ const Events = [
   {
     id: 3,
     name: 'ng-conf 2037',
-    date: '5/4/2037',
+    date: new Date('5/4/2037'),
     time: '9:00 am',
     price: 759.00,
     imageUrl: '/assets/images/ng-conf.png',
@@ -238,7 +254,7 @@ const Events = [
   {
     id: 4,
     name: 'UN Angular Summit',
-    date: '6/10/2037',
+    date: new Date('6/10/2037'),
     time: '8:00 am',
     price: 800.00,
     imageUrl: '/assets/images/basic-shield.png',
@@ -287,7 +303,7 @@ const Events = [
   {
     id: 5,
     name: 'ng-vegas',
-    date: '2/10/2037',
+    date: new Date('2/10/2037'),
     time: '9:00 am',
     price: 400.00,
     imageUrl: '/assets/images/ng-vegas.png',
