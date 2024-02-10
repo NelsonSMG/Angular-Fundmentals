b3e0d1daf634f536a7a43e97d01206e63a530357
NelsonSMG <nelsonsmalfitano@gmail.com>
2024-02-10 00:47:09 -0300

    auth service and navbar component

--- src/app/nav/navbar/navbar.component.ts
+++ src/app/nav/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/user/auth.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,14 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styles: [
     `
       .nav.navbar-nav {font-size: 15px}
+      li > a.active { color:#F97924 }
       #searchForm {margin-right: 100px}
-      @media (max-width: 1200px) {#searchForm {display: none}}
+      @media (max-width: 1200px) {#searchForm {display:none}}
     `
   ]
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
